52ef59f79d12bc73e376e5cc58ab459315d988cd
Ilia Pshenichkin <psilya@msn.com>
2020-07-02 11:16:16 +0300

    Canvas First Look

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { shape: 'something shape' })
-})
+  res.render('index', { shape: 'something shape' });
+});
 
 module.exports = router;
